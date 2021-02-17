56d0096f87b326d91f40190ac62bc520cfde90b6
Arkandias <arkandias1@hotmail.fr>
2021-02-17 16:02:10 +0100

    Change transposition to use insertion mode

--- src/components/DraggableTranspositionLetter.js
+++ src/components/DraggableTranspositionLetter.js
@@ -4,20 +4,31 @@ import {useDrag, useDrop} from 'react-dnd';
 export const DraggableTranspositionLetter = ({letter, index, moveLetter, locked}) => {
   const ref = useRef(null);
 
-  const [{canDrop, isOver}, drop] = useDrop({
+  const [, drop] = useDrop({
     accept: 'transposition-letter',
     collect: (monitor) => ({
       isOver: monitor.isOver(),
       canDrop: monitor.canDrop(),
     }),
-    drop: (item) => {
+    hover (item, monitor) {
+      if (!ref.current) {
+        return;
+      }
       const dragIndex = item.index;
       const hoverIndex = index;
-      // Don't replace items with themselves
       if (dragIndex === hoverIndex) {
         return;
       }
-
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+      const hoverMiddleX = (hoverBoundingRect.right - hoverBoundingRect.left) / 2;
+      const clientOffset = monitor.getClientOffset();
+      const hoverClientX = clientOffset.x - hoverBoundingRect.left;
+      if (dragIndex < hoverIndex && hoverClientX < hoverMiddleX) {
+        return;
+      }
+      if (dragIndex > hoverIndex && hoverClientX > hoverMiddleX) {
+        return;
+      }
       moveLetter(dragIndex, hoverIndex);
       item.index = hoverIndex;
     },
@@ -32,11 +43,9 @@ export const DraggableTranspositionLetter = ({letter, index, moveLetter, locked}
   const opacity = isDragging ? 0 : 1;
   drag(drop(ref));
 
-  const isActive = canDrop && isOver;
-
   return (
     <div ref={ref} className="transposition-slot" style={{opacity}}>
-      <div className={`transposition-letter letter-cell ${isActive ? 'substitution-letter-hover' : ''} ${locked ? 'is-locked' : ''}`}>
+      <div className={`transposition-letter letter-cell ${locked ? 'is-locked' : ''}`}>
         <span>{letter}</span>
       </div>
     </div>
